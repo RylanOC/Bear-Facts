2688614e103dcac115bf9292dc92ec01f310bcf5
Rylan <rylanoc@gmail.com>
2020-05-02 23:19:27 -0400

    add json configuration options

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use serde_json::Value;
 use serenity::{model::prelude::*, prelude::*, Client};
+use std::fs;
 
 struct Handler;
 
@@ -6,6 +8,12 @@ impl EventHandler for Handler {
     fn message(&self, context: Context, msg: Message) {
         unimplemented!();
     }
+}
+
+fn read_config(path: &str) -> Value {
+    let contents = fs::read_to_string(path).expect("[!] Error reading config file!");
+    let config = serde_json::from_str(&contents).expect("[!] Error parsing JSON config!");
+    return config;
 }
 
 fn main() {
